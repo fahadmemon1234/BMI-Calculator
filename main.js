f91ab96b0afaa557f6d3f4e0368cb6e95f77c7e1
fahadmemon1234 <fahadmemon131@gmail.com>
2024-03-24 15:06:52 +0500

    add file

---
+++ main.js
@@ -0,0 +1,5 @@
+// BMI Calculator
+var weightInKg = 30;
+var heightInMeters = 3.45;
+var bmi = weightInKg / (heightInMeters * heightInMeters);
+console.log("Your BMI is ".concat(bmi));
---
+++ main.ts
@@ -0,0 +1,6 @@
+// BMI Calculator
+
+let weightInKg = 30;
+let heightInMeters = 3.45;
+let bmi = weightInKg / (heightInMeters * heightInMeters);
+console.log(`Your BMI is ${bmi}`);
